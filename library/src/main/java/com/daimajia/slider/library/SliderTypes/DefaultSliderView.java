ac56161a590b2da9da9c91367d87ffa554bccaa5
Joey Vadala <joey@joeyvadala.com>
2017-01-05 21:09:32 -0600

    Update DefaultSliderView.java
    TouchImageView DefaultImageSlider

--- library/src/main/java/com/daimajia/slider/library/SliderTypes/DefaultSliderView.java
+++ library/src/main/java/com/daimajia/slider/library/SliderTypes/DefaultSliderView.java
@@ -21,7 +21,7 @@ public class DefaultSliderView extends BaseSliderView{
     @Override
     public View getView() {
         View v = LayoutInflater.from(getContext()).inflate(R.layout.render_type_default,null);
-        ImageView target = (ImageView)v.findViewById(R.id.daimajia_slider_image);
+        TouchImageView target = (ImageView)v.findViewById(R.id.daimajia_slider_image);
         bindEventAndShow(v, target);
         return v;
     }
